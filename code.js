a29749308e80aa1bb6b3edb8b7fc64ea82cdb3f2
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-05-01 16:10:52 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,21 @@
 function augmentingPath(graph, start, end) {
-    return [];
+    var visitedNodes = [];
+    function recursive(node){
+        if (node === end){
+            return [node];
+        }
+        visitedNodes.push(node);
+        for (var edge in graph[node]){
+            if (!visitedNodes.includes(edge)){
+                if (graph[node][edge] > 0){
+                    var path = recursive(edge);
+                    if (path.length > 0){
+                        return [node].concat(path);
+                    }
+                }
+            }
+        }
+        return [];
+    }
+    return recursive(start);
 }
